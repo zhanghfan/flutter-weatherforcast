d9afe0dac07e2f9c13145c0e1a5b35254612baea
zhangfan <15513807952@163.com>
2018-12-24 16:41:36 +0800

    first commit

---
+++ lib/city/CityWidget.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_hello/city/CityData.dart';
+import 'package:flutter_hello/weather/WeatheerWidget.dart';
+import 'package:http/http.dart' as http;
+class CityWidget extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return CityState();
+  }
+
+}
+
+class CityState extends State<CityWidget>{
+
+  List<CityData> cityList = new List<CityData>();
+
+  CityState(){
+    _getCityList();
+  }
+
+  void _getCityList() async{
+    List<CityData> citys = await _fetchCityList();
+    setState(() {
+      cityList = citys;
+    });
+  }
+
+  //拉取城市列表
+  Future<List<CityData>> _fetchCityList() async{
+    final response = await http.get('https://search.heweather.net/top?group=cn&key=ebb698e9bb6844199e6fd23cbb9a77c5');
+
+    List<CityData> cityList = new List<CityData>();
+
+    if(response.statusCode == 200){
+      //解析数据
+      Map<String,dynamic> result = json.decode(response.body);
+      for(dynamic data in result['HeWeather6'][0]['basic']){
+        CityData cityData = CityData(data['location']);
+        cityList.add(cityData);
+      }
+      return cityList;
+    }else{
+      return cityList;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListView.builder(
+        itemCount: cityList.length,
+        itemBuilder: (context,index){
+          return ListTile(
+            title: GestureDetector(
+              child:  Text(cityList[index].cityName),
+              onTap:(){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => WeatherWidget(cityList[index].cityName))
+                );
+              },
+            ),
+          );
+        });
+  }
+
+}
---
+++ lib/weather/WeatheerWidget.dart
@@ -0,0 +1,108 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'WeatherData.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherWidget extends StatefulWidget{
+  String cityName;
+
+  WeatherWidget(this.cityName);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new WeatherState(this.cityName);
+  }
+
+}
+
+class WeatherState extends State<WeatherWidget>{
+
+  String cityName;
+
+  WeatherData weather = WeatherData.empty();
+
+  WeatherState(String cityName){
+    this.cityName = cityName;
+    _getWeather();
+  }
+
+  void _getWeather() async{
+    WeatherData data = await _fetchWeather();
+    setState((){
+      weather = data;
+    });
+  }
+
+  Future<WeatherData> _fetchWeather() async{
+    final response = await http.get('https://free-api.heweather.com/s6/weather/now?location='+this.cityName+'&key=ebb698e9bb6844199e6fd23cbb9a77c5');
+    if(response.statusCode == 200){
+      return WeatherData.fromJson(json.decode(response.body));
+    }else{
+      return WeatherData.empty();
+    }
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset("images/weather_bg.jpg",fit: BoxFit.fitHeight,),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(top: 40.0),
+                child: new Text(
+                  this.cityName,
+                  textAlign: TextAlign.center,
+                  style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(top: 100.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                        weather?.tmp,
+                        style: new TextStyle(
+                            color: Colors.white,
+                            fontSize: 80.0
+                        )),
+                    Text(
+                        weather?.cond,
+                        style: new TextStyle(
+                            color: Colors.white,
+                            fontSize: 45.0
+                        )),
+                    Text(
+                      weather?.hum,
+                      style: new TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+}
